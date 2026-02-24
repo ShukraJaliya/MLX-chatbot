0728cc75039cfb96afdc6d251e048e950315d47f
ShukraJaliya <nankabirwas@spu.edu>
2026-02-23 17:15:25 -0800

    Initial commit

---
+++ MLX-chatbot/MLX_chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLX_chatbotApp.swift
+//  MLX-chatbot
+//
+//  Created by AVLA Student on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct MLX_chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
